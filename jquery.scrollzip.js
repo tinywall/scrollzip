e9056adfa3e22ebe5be0da9ceff683dbbcdcbb09
Arun David <arundavid.info@gmail.com>
2013-10-04 22:24:08 +0530

    Initial commit

---
+++ jquery.scrollzip.js
@@ -0,0 +1,45 @@
+/**
+* jQuery scrollzip Plugin 1.0
+*
+* http://www.tinywall.net/
+*
+* Copyright (c) 2013 Arun David
+*/
+(function($){
+    $.fn.scrollzipInit=function(){
+        $('body').prepend("<div style='position:fixed;top:0px;left:0px;width:0;height:0;' id='scrollzipPoint'></div>" );
+    };
+    $.fn.scrollzip = function(options){
+        var settings = $.extend({
+            showFunction    : null,
+            hideFunction    : null,
+            showShift       : 0,
+            wholeVisible    : false,
+            hideShift       : 0,
+        }, options);
+        return this.each(function(i,obj){
+            $(this).addClass('scrollzip');
+            if ( $.isFunction( settings.showFunction ) ){
+                if(
+                    !$(this).hasClass('isShown')&&
+                    ($(window).outerHeight()+$('#scrollzipPoint').offset().top-settings.showShift)>($(this).offset().top+((settings.wholeVisible)?$(this).outerHeight():0))&&
+                    ($('#scrollzipPoint').offset().top+((settings.wholeVisible)?$(this).outerHeight():0))<($(this).outerHeight()+$(this).offset().top-settings.showShift)
+                ){
+                    $(this).addClass('isShown');
+                    settings.showFunction.call( this );
+                }
+            }
+            if ( $.isFunction( settings.hideFunction ) ){
+                if(
+                    $(this).hasClass('isShown')&&
+                    (($(window).outerHeight()+$('#scrollzipPoint').offset().top-settings.hideShift)<($(this).offset().top+((settings.wholeVisible)?$(this).outerHeight():0))||
+                    ($('#scrollzipPoint').offset().top+((settings.wholeVisible)?$(this).outerHeight():0))>($(this).outerHeight()+$(this).offset().top-settings.hideShift))
+                ){
+                    $(this).removeClass('isShown');
+                    settings.hideFunction.call( this );
+                }
+            }
+            return this;
+        });
+    };
+})(jQuery);
